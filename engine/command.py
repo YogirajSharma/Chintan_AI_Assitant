4c6cb6e50c3cf47adbb988d7dbfec64419d88913
YogirajSharma <yogirajsharma482@gmail.com>
2025-02-16 10:50:00 +0530

    speak j

--- engine/command.py
+++ engine/command.py
@@ -1,6 +1,6 @@
 import pyttsx3 #pip install pyttsx3
 import speech_recognition as sr #pip install SpeechRecognition
-
+import eel
 
 def speak(text):
     text = str(text)
@@ -12,13 +12,15 @@ def speak(text):
     engine.say(text)
     #eel.receiverText(text)
     engine.runAndWait()
+
+@eel.expose
 def takecommand():
 
     r = sr.Recognizer()
 
     with sr.Microphone() as source:
         print('listening....')
-       # eel.DisplayMessage('listening....')
+        eel.DisplayMessage('listening....')
         r.pause_threshold = 1
         r.adjust_for_ambient_noise(source)
         
@@ -26,15 +28,15 @@ def takecommand():
 
     try:
         print('recognizing')
-        #eel.DisplayMessage('recognizing....')
+        eel.DisplayMessage('recognizing....')
         query = r.recognize_google(audio, language='en-in')
         print(f"user said: {query}")
-        #eel.DisplayMessage(query)
+        eel.DisplayMessage(query)
         #time.sleep(2)
+        speak(query)
+        eel.ShowHood()
        
     except Exception as e:
         return ""
     
     return query.lower()
-text = takecommand()
-speak(text)
